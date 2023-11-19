fa187c3b85bdc3f5f025ca3a419797696e217843
Vitalii Fetisov <neverest1827@gmail.com>
2023-11-19 20:33:16 +0200

    Added the getSqlCommand function and partially rewrote the getBooks function

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,18 +1,25 @@
-import {TypeBooks} from "types";
-import { connection } from "./database.js";
+import mysql, { Connection } from "mysql2/promise";
+import { TypeBooks } from "types";
 import fs from "fs/promises";
 
+const config: string = await fs.readFile('./config.json', 'utf-8');
 const default_offset: number = 20;
-export async function getBooks(offset: number | undefined = default_offset, filter?: string): Promise<TypeBooks[] | null> {
+export async function getBooks(filter: string, offset: string | number, limit: string): Promise<TypeBooks[] | null> {
+    offset = offset ? offset : default_offset;
+    const connection: Connection = await mysql.createConnection(JSON.parse(config));
     try {
         await connection.connect()
-        const sql_command: string = await fs.readFile('./dist/sql/getBooks.sql', 'utf-8');
-        const data = await connection.execute(sql_command);
-        return data[0] as TypeBooks[];
+        const sql_command: string = await getSqlCommand(filter)
+        const [result , field] = await connection.execute(sql_command);
+        return result as TypeBooks[];
     } catch (err) {
         console.log(`${err}`)
         return null;
     } finally {
         await connection.end()
     }
+}
+
+async function getSqlCommand(filter: string): Promise<string> {
+    return await fs.readFile(`./dist/sql/get_books_filter_${filter}.sql`, 'utf-8');
 }
