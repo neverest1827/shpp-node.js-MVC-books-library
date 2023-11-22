26d314f62139a0d284900dc300c16f6e2fc4d067
Vitalii Fetisov <neverest1827@gmail.com>
2023-11-22 06:32:21 +0200

    realized pagination

--- static/books-page_files/index.js
+++ static/books-page_files/index.js
@@ -1,11 +1,13 @@
-var drawItemsOnScroll,
-    isScrollRunning = false;
-console.log(isScrollRunning);
+var drawItemsOnClick;
+// var drawItemsOnScroll,
+//     isScrollRunning = false;
+// console.log(isScrollRunning);
 
 $(document).ready(function () {
 
-    (function () {
+    const [prev, next] = document.querySelectorAll('.pagination__link');
 
+    (function () {
         data = {
             filter: getParameterByName('filter') || "new",
             offset: getParameterByName('offset'),
@@ -16,7 +18,9 @@ $(document).ready(function () {
         doAjaxQuery('GET', '/api/v1/books', data, function (res) {
             console.log('qindex');
             view.addBooksItems(res.data.books, true);
-            drawItemsOnScroll = initDrawItemsOnScroll(res.data.total.amount);
+            // drawItemsOnScroll = initDrawItemsOnScroll(res.data.total.amount);
+            drawItemsOnClick = initDrawItemsOnClick(res.data.total.amount);
+            showButton(next, prev, res.data.offset, res.data.total.amount)
             if (localStorage.getItem('h')) {
                 $(window).scrollTop(localStorage.getItem('h'));
                 localStorage.removeItem('h');
@@ -28,12 +32,25 @@ $(document).ready(function () {
         localStorage.setItem('h', $(window).scrollTop());
     });
 
-    $(document).scroll(function () {
-        if ((( $(document).height() - $(window).scrollTop() ) < ( 2 * $(window).height() )) && !isScrollRunning) {
-            isScrollRunning = true;
-            drawItemsOnScroll();
-        }
-    });
+    // $(document).scroll(function () {  // Пагінація по скролу
+    //     if ((( $(document).height() - $(window).scrollTop() ) < ( 2 * $(window).height() )) && !isScrollRunning) {
+    //         isScrollRunning = true;
+    //         drawItemsOnScroll();
+    //     }
+    // });
+
+    const handleNextClick = (e) => {
+        e.preventDefault();
+        drawItemsOnClick(true);
+    };
+
+    const handlePrevClick = (e) => {
+        e.preventDefault();
+        drawItemsOnClick(false);
+    };
+
+    prev.addEventListener('click', handlePrevClick);
+    next.addEventListener('click', handleNextClick);
 });
 
 function getParameterByName(name, url) {
@@ -47,36 +64,75 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-var initDrawItemsOnScroll = function (maxItems) {
+// var initDrawItemsOnScroll = function (maxItems) {
+//     var maxNumOfItems = maxItems,
+//         limit = global.number_of_items_onscroll,
+//         offset = parseInt(getParameterByName('count')) || global.items_limit_on_page_load;
+//
+//     return function () {
+//         if (offset < maxNumOfItems) {
+//             var data = {
+//                 'filter': getParameterByName('filter') || "new",
+//                 'limit': limit,
+//                 'offset': offset
+//             };
+//             $("#loading").slideDown();
+//             doAjaxQuery('GET', '/api/v1/books', data, function (res) {
+//                     $("#loading").slideUp();
+//                     isScrollRunning = false;
+//                     view.addBooksItems(res.data.books, false);
+//                     changeHistoryStateWithParams("replace", res.data.filter, res.data.offset);
+//                 });
+//             offset += limit;
+//         }
+//     }
+// };
+
+var initDrawItemsOnClick = function (maxItems) {
     var maxNumOfItems = maxItems,
         limit = global.number_of_items_onscroll,
         offset = parseInt(getParameterByName('count')) || global.items_limit_on_page_load;
+    const [prev, next] = document.querySelectorAll('.pagination__link');
 
-    return function () {
-        if (offset < maxNumOfItems) {
-            var data = {
-                'filter': getParameterByName('filter') || "new",
-                'limit': limit,
-                'offset': offset
-            };
-            $("#loading").slideDown();
-            doAjaxQuery('GET', '/api/v1/books', data,
-                function (res) {
-                    $("#loading").slideUp();
-                    isScrollRunning = false;
-                    view.addBooksItems(res.data.books, false);
-                    changeHistoryStateWithParams("replace", res.data.filter, res.data.offset);
-                });
+    return function (boolean) {
+
+        if (!boolean) {
+            offset -= limit;
+        }
+
+        var data = {
+            'filter': getParameterByName('filter') || "new",
+            'limit': limit,
+            'offset': offset
+        };
+        $("#loading").slideDown();
+        doAjaxQuery('GET', '/api/v1/books', data, function (res) {
+            $("#loading").slideUp();
+            isScrollRunning = false;
+            if (boolean) {
+                view.addBooksItems(res.data.books, false);
+            } else {
+                view.removeBooksItems(res.data.books);
+            }
+            changeHistoryStateWithParams("replace", res.data.filter, res.data.offset, boolean);
+        });
+
+        if (boolean) {
             offset += limit;
         }
+
+        showButton(next, prev, offset, maxItems)
     }
 };
 
 function loadIndexPage(reqData) {
     doAjaxQuery('GET', '/api/v1/books', reqData, function (res) {
         view.addBooksItems(res.data.books, true);
-        changeHistoryStateWithParams('push', res.data.filter, res.data.offset);
-        drawItemsOnScroll = initDrawItemsOnScroll(res.data.total.amount);
+        changeHistoryStateWithParams('push', res.data.filter, res.data.offset, true);
+        // drawItemsOnScroll = initDrawItemsOnScroll(res.data.total.amount);
+        drawItemsOnClick = initDrawItemsOnClick(res.data.total.amount);
+        const [prev, next] = document.querySelectorAll('.pagination__link');
+        showButton(next, prev, res.data.offset, res.data.total.amount)
     });
 }
 
@@ -87,5 +143,19 @@ function setSidebarActiveButton(activeElem, filterStringValue) {
         return;
     } else {
         $('a[data-filter=' + filterStringValue + ']').addClass('active');
+    }
+}
+
+function showButton(next, prev, offset, maxItems) {
+    if (offset < maxItems) {
+        next.classList.add('pagination__link--visible');
+    } else {
+        next.classList.remove('pagination__link--visible');
+    }
+
+    if (offset > global.items_limit_on_page_load) {
+        prev.classList.add('pagination__link--visible');
+    } else {
+        prev.classList.remove('pagination__link--visible');
     }
 }
