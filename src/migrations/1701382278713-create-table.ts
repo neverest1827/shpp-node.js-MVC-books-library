c289329896ea35b427469a40daef2a1045834b97
Vitalii Fetisov <neverest1827@gmail.com>
2023-12-06 08:39:54 +0200

    implemented the first migration file

--- src/migrations/1701382278713-create-table.ts
+++ src/migrations/1701382278713-create-table.ts
@@ -1,10 +1,37 @@
+import fs from "fs/promises";
+import mysql, {Connection} from "mysql2/promise";
+const path_to_class: string = "../classes/data_base" || "./data_base.js"
+
 export default {
 
     async up(): Promise<void>{
-        console.log('up')
+
+        const sql_script: string = await fs.readFile('./sql/create_table_v1.sql', 'utf-8');
+        const path_to_config: string = "../config.json";
+        const config: string = await fs.readFile(path_to_config, 'utf-8');
+        const connection: Connection = await mysql.createConnection(JSON.parse(config));
+
+        try {
+            await connection.execute(sql_script)
+        } catch (err){
+            console.log(err)
+        } finally {
+            await connection.end()
+        }
     },
 
     async down(): Promise<void>{
-        console.log('down')
+        const sql_script: string = await fs.readFile('./sql/drop_table_v1.sql', 'utf-8');
+        const path_to_config: string = "../config.json";
+        const config: string = await fs.readFile(path_to_config, 'utf-8');
+        const connection: Connection = await mysql.createConnection(JSON.parse(config));
+
+        try {
+            await connection.execute(sql_script);
+        } catch (err){
+            console.log(err)
+        } finally {
+            await connection.end();
+        }
     }
 }
