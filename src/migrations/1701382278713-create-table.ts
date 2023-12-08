e6456baa24d587a9732eb887a73e63248e5bd6aa
Vitalii Fetisov <neverest1827@gmail.com>
2023-12-08 11:47:40 +0200

    modernized the first migration file

--- src/migrations/1701382278713-create-table.ts
+++ src/migrations/1701382278713-create-table.ts
@@ -1,37 +1,49 @@
-import fs from "fs/promises";
-import mysql, {Connection} from "mysql2/promise";
-const path_to_class: string = "../classes/data_base" || "./data_base.js"
+import {TypeData} from "types";
 
+const path_to_class: string = "../classes/data_base.js" || "./data_base.js"
+const { DataBase } = await import(path_to_class);
+// import {DataBase} from "../classes/data_base";
 export default {
 
     async up(): Promise<void>{
-
-        const sql_script: string = await fs.readFile('./sql/create_table_v1.sql', 'utf-8');
-        const path_to_config: string = "../config.json";
-        const config: string = await fs.readFile(path_to_config, 'utf-8');
-        const connection: Connection = await mysql.createConnection(JSON.parse(config));
-
         try {
-            await connection.execute(sql_script)
-        } catch (err){
-            console.log(err)
-        } finally {
-            await connection.end()
+            const handlerDb = await DataBase.getInstance();
+            const sql_script: string = await handlerDb.getSqlScript('create_table_v1.sql');
+            const sql_fill_script = await handlerDb.getSqlScript('fill_table_v1.sql');
+            await handlerDb.execute(sql_script);
+
+            const books = await handlerDb.parseCsv()
+
+            for(const key of Object.keys(books)){
+                const book = books[key];
+                const date = book.date ? book.date : new Date(Date.now()).toDateString();
+                await handlerDb.execute(sql_fill_script, [
+                    book.isbn ? book.isbn : null,
+                    book.title,
+                    book.author,
+                    book.description,
+                    book.year,
+                    book.pages ? book.pages : null,
+                    book.stars ? book.stars : null,
+                    date.slice(0, 19).replace("T", " "),
+                    book.clicks ? book.clicks : null,
+                    book.views ? book.views : null,
+                    !!book.event,
+                ]);
+            }
+        } catch (err) {
+            throw new Error(`${err}`);
         }
     },
 
     async down(): Promise<void>{
-        const sql_script: string = await fs.readFile('./sql/drop_table_v1.sql', 'utf-8');
-        const path_to_config: string = "../config.json";
-        const config: string = await fs.readFile(path_to_config, 'utf-8');
-        const connection: Connection = await mysql.createConnection(JSON.parse(config));
-
         try {
-            await connection.execute(sql_script);
+            const handlerDb = await DataBase.getInstance();
+            const sql_script: string = await handlerDb.getSqlScript('drop_table_v1.sql');
+            await handlerDb.exportToCsv('library');
+            await handlerDb.execute(sql_script);
         } catch (err){
-            console.log(err)
-        } finally {
-            await connection.end();
+            throw new Error(`${err}`);
         }
     }
 }
