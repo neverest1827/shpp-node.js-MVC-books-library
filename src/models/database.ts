0d4f73dfebf7c91844c690de6065e2a61bfc6c32
Vitalii Fetisov <neverest1827@gmail.com>
2023-12-13 04:43:03 +0200

    deleted unnecessary files

--- src/models/database.ts
+++
@@ -1,6 +0,0 @@
-import mysql, {Connection} from "mysql2/promise";
-import fs from "fs/promises";
-
-const config: string = await fs.readFile('./config.json', 'utf-8');
-export const connection: Connection = await mysql.createConnection(JSON.parse(config));
-
