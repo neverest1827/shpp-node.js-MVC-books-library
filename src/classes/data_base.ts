d48435023c929506fc580ebb82413b91ad371f43
Vitalii Fetisov <neverest1827@gmail.com>
2023-12-13 04:52:47 +0200

    implemented the use of a dynamic path to avoid conflicts and added the getCurrentMigration method and modernized the getSqlScript method

--- src/classes/data_base.ts
+++ src/classes/data_base.ts
@@ -1,25 +1,30 @@
 import fs from "fs/promises";
-import mysql, {Connection, ResultSetHeader, RowDataPacket} from "mysql2/promise";
+import mysql, {Connection, RowDataPacket} from "mysql2/promise";
 import fast_csv from "fast-csv";
 import {TypeData} from "types";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 export class DataBase {
     private static instance: DataBase | null = null;
     config: object
+    path_to_work_dir: string
     path_to_sql_scripts: string
     sql_scripts: {[key: string]: string}
-    constructor(config: object, path_to_sql_scripts: string) {
+
+    constructor(config: object, path_to_work_dir: string) {
         this.config = config;
-        this.path_to_sql_scripts = path_to_sql_scripts;
+        this.path_to_work_dir = path_to_work_dir;
+        this.path_to_sql_scripts = path_to_work_dir + 'dist/sql/';
         this.sql_scripts = {};
     }
 
     static async getInstance(): Promise<DataBase> {
         if (!DataBase.instance) {
-            const path_to_sql_scripts: string = "./sql/";
-            const path_to_config: string = "../config.json";
-            const config: string = await fs.readFile(path_to_config, 'utf-8');
-            DataBase.instance = new DataBase(JSON.parse(config), path_to_sql_scripts);
+            const path_to_file: string = path.dirname(fileURLToPath(import.meta.url));
+            const path_to_work_dir: string = path_to_file.substring(0, path_to_file.indexOf('dist'));
+            const config: string = await fs.readFile(path_to_work_dir + 'config.json', 'utf-8');
+            DataBase.instance = new DataBase(JSON.parse(config), path_to_work_dir);
         }
         return DataBase.instance;
     }
@@ -32,7 +37,7 @@ export class DataBase {
         }
     }
 
-    async execute(sqlScript: string, data?: any){
+    async execute(sqlScript: string, data?: any[]){
         const connection: Connection = await this.createConnection()
         try {
             if (data && data.length) return await connection.execute(sqlScript, data);
@@ -46,20 +51,21 @@ export class DataBase {
     }
 
 
-    async getSqlScript(fileName: string): Promise<string> {
+    async getSqlScript(fileName: string, versionFolder?: string | undefined): Promise<string> {
         if (Object.keys(this.sql_scripts).includes(fileName)) return this.sql_scripts[fileName]
+        versionFolder = versionFolder ? versionFolder : '';
         try {
-            const sql_script: string = await fs.readFile(this.path_to_sql_scripts + fileName, 'utf-8');
+            const sql_script: string =
+                await fs.readFile(`${this.path_to_sql_scripts + versionFolder}/${fileName}`, 'utf-8');
             this.sql_scripts[fileName] = sql_script;
             return sql_script;
         } catch (err) {
-            console.log(err);
             throw new Error(`${err}`);
         }
     }
 
     async importFromCsv(tableName: string, csvData: [string | number| boolean]): Promise<void> {
-        const sql_script = await this.getSqlScript('fill_table_v1.sql');
+        const sql_script = await this.getSqlScript('insert_new_book.sql', 'v1');
         await this.execute(sql_script, csvData);
     }
 
@@ -97,6 +103,17 @@ export class DataBase {
             throw new Error(`${err}`);
         } finally {
             await connection.end();
+        }
+    }
+
+    async getCurrentMigration(): Promise<string | undefined> {
+        try {
+            const data: string =
+                await fs.readFile(this.path_to_work_dir + 'dist/migrate_log.json', 'utf-8');
+            const migration = JSON.parse(data);
+            return migration.name;
+        } catch (err) {
+            return undefined;
         }
     }
 }
