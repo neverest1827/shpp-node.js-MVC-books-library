2b444f39d3d21433d4a91266a240be6698318189
Vitalii Fetisov <neverest1827@gmail.com>
2023-12-15 09:45:52 +0200

    path fixing

--- src/migrator.ts
+++ src/migrator.ts
@@ -1,10 +1,13 @@
 import {Migrate} from "./classes/migrate.js";
 import {DataBase} from "./classes/data_base.js";
+import path from "path";
+import {fileURLToPath} from "url";
 
 type MigrateMethod = 'create' | 'up' | 'down' | 'ls';
 
 const dataBase: DataBase = await DataBase.getInstance();
-const path_to_migrations: string = './migrations/';
+const path_to_file: string = path.dirname(fileURLToPath(import.meta.url));
+const path_to_migrations: string = path.join(path_to_file, 'migrations');
 const migrate: Migrate = await Migrate.getInstance(dataBase, path_to_migrations)
 
 const args: string[] = process.argv.slice(2);
