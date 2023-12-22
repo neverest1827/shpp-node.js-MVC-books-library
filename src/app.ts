29362ba7f0b96ee020bd87bd79e7f88319aa3ae9
Vitalii Fetisov <neverest1827@gmail.com>
2023-12-22 21:46:50 +0200

    added a path for catching 404s and returning a standard photo if the target photo does not exist

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,45 @@
-import express, {Express} from "express"
-import { fileURLToPath } from 'url';
-import bodyParser from "body-parser";
-import path from "path";
-import {user_router} from "./routes/user_router.js";
+import {AppError, APP_PATHS, APP_PORT} from "./configs/app_config.js";
+import express, {Express, Request, Response} from "express"
 import {admin_router} from "./routes/admin_router.js";
-import {tasks} from "./cron.js";
+import {user_router} from "./routes/user_router.js";
+import bodyParser from "body-parser";
+import {cron_tasks} from "./cron.js";
+import path from "path";
 
-const __filename: string = fileURLToPath(import.meta.url);
-const __dirname: string = path.dirname(__filename);
 const app: Express = express();
-const port: number = 3000;
 
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(express.static(APP_PATHS.PATH_TO_STATIC_FOLDER));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(admin_router);
 app.use(user_router);
-app.use(admin_router)
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.set('views', APP_PATHS.PATH_TO_VIEWS_FOLDER);
 
-for (const task of tasks){
+/**
+ * Route for processing the error of receiving the image from the server and reversing the default image
+ */
+app.get('/books-images/:id', (req: Request, res: Response): void => {
+    res.sendFile(path.join(APP_PATHS.PATH_TO_IMAGES, 'not-found.jpg'));
+});
+
+app.all('*', (req: Request, res: Response) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile('404.ejs');
+    } else if (req.accepts('json')) {
+        res.json({"error": AppError.FILE_NOT_FOUND});
+    } else {
+        res.type('txt').send(AppError.FILE_NOT_FOUND);
+    }
+})
+
+// Starting Cron tasks
+for (const task of cron_tasks){
     task.start();
 }
 
-app.listen(port, () => {
-    console.log(`Server listen the port ${port}`)
+app.listen(APP_PORT, () => {
+    console.log(`Server listen the port ${APP_PORT}`)
 })
